940177cb15fbe4f66aab754909d0d43fe1694664
Tibor Koderman <50010022+TiborKoderman@users.noreply.github.com>
2023-03-29 00:07:01 +0200

    Delete build directory

--- build/exercise1/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tibor/repos/a-tech-titan/src/exercise1
-# Build directory: /home/tibor/repos/a-tech-titan/build/exercise1
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
