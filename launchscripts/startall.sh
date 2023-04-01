0cf20d76d41ea0f11966130d92dac9cc70ac28c5
Tibor Koderman <tibor.koderman@gmail.com>
2023-04-01 21:24:11 +0200

    added some launch scripts

--- launchscripts/startall.sh
+++ launchscripts/startall.sh
@@ -2,10 +2,10 @@
 
 cd ..
 
-gnome-terminal -- roslaunch exercise3 rins_world.launch &
+gnome-terminal -- roslaunch exercise3 rins_world.launch
 
-sleep 5
+sleep 10
 
-gnome-terminal -- roslaunch exercise3 amcl_simulation.launch &
+gnome-terminal -- roslaunch exercise3 amcl_simulation.launch
 
-gnome-terminal -- roslaunch turtlebot_rviz_launchers view_navigation.launch &
+gnome-terminal -- roslaunch turtlebot_rviz_launchers view_navigation.launch
