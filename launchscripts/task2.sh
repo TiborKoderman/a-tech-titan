2387a4b62943dc3a87aac5d47e38f014696fb445
jernejakrajcar <jerneja.krajcar@gmail.com>
2023-05-02 18:54:54 +0200

    fix error

--- launchscripts/task2.sh
+++ launchscripts/task2.sh
@@ -8,5 +8,4 @@ gnome-terminal -- roslaunch turtlebot_custom_navigation amcl_simulation.launch
 gnome-terminal -- roslaunch turtlebot_rviz_launchers view_navigation.launch
 gnome-terminal -- rosrun exercise6 detect_rings
 gnome-terminal -- rosrun exercise6 move_arm.py
-gnome-terminal -- rosrun exercise6 waypoint_generator.py
 gnome-terminal -- roslaunch exercise6 find_cylinder.launch
