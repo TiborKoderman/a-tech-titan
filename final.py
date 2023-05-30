37a13bb7ca42f6234114674cdd22cf3027759350
javuxi <en.janijani@gmail.com>
2023-05-30 13:57:16 +0200

    added face recognition

--- final.py
+++ final.py
@@ -24,6 +24,29 @@ def detect_posters(rgb_image):
 
     boxes = face_recognition.face_locations(rgb_image, model="hog")
     encodings = face_recognition.face_encodings(rgb_image, boxes)
+    object_type = ""
+
+    for encoding in encodings:
+        matches = face_recognition.compare_faces(data["encodings"],
+            encoding)
+        name = "Unknown"
+        if True in matches:
+            matchedIdxs = [i for (i, b) in enumerate(matches) if b]
+            counts = {}
+            for i in matchedIdxs:
+                name = data["names"][i]
+                counts[name] = counts.get(name, 0) + 1
+            name = max(counts, key=counts.get)
+        object_type = name
+
+    return object_type
+
+def detect_poster_faces(rgb_image):
+
+    data = pickle.loads(open("encodings_poster_faces.pickle", "rb").read())
+
+    boxes = face_recognition.face_locations(rgb_image, model="hog")
+    encodings = face_recognition.face_encodings(rgb_image, boxes)
     final_name = ""
 
     for encoding in encodings:
